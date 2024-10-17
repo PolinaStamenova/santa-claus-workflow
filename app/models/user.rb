1a64ae984d355be18e03d64f6978319d3d4d842c
Polina Stamenova <polina.stamenova@gmail.com>
2024-10-17 15:39:57 +0300

    Add role to User:
    Setup default 'elf' role

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # Associations
+
+  # Validations
   validates :name, presence: true
+
+  def santa?
+    role == 'santa'
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
   end
+
+  describe '#santa?' do
+    it 'returns true' do
+      user = User.new(name: 'Santa', email: 'santa@test.com', password: '123456', role: 'santa')
+      user.save!
+
+      expect(user.santa?).to be(true)
+    end
+  end
 end
